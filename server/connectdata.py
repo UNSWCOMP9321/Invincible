bd0c4a6c8d1cbb59d4b5bceae2a02c569f517d26
pacjn <pachenjunnan@163.com>
2018-05-18 20:41:02 +1000

    add more database connections

--- server/connectdata.py
+++ server/connectdata.py
@@ -7,8 +7,44 @@ conn = sqlite3.connect(dbpath,check_same_thread=False)
 cu=conn.cursor()
 
 #{location: {'city1':[x,y], 'city2':[x,y],...}, data:[{name: 'city1', value:233}, {name: 'city2', value:500}]}
-def get_CITY_AU():
-    cu.execute("select * from CITY_AU")
+def get_CITY_AU(limit=15):
+    cu.execute("select * from CITY_AU ORDER BY job_amount DESC limit "+str(limit))
+    loc_dic={}
+    data=[]
+    for c in cu.fetchall():
+        loc_dic[c[0]]=(c[1],c[2])
+        data.append({"name":c[0],"value":c[3]})
+    return {"location":loc_dic,"data":data}
+
+def get_CITY_UK(limit=15):
+    cu.execute("select * from CITY_UK ORDER BY job_amount DESC limit "+str(limit))
+    loc_dic={}
+    data=[]
+    for c in cu.fetchall():
+        loc_dic[c[0]]=(c[1],c[2])
+        data.append({"name":c[0],"value":c[3]})
+    return {"location":loc_dic,"data":data}
+
+def get_CATE_AU(limit=15):
+    cu.execute("select * from CATE_AU limit "+str(limit))
+    loc_dic={}
+    data=[]
+    for c in cu.fetchall():
+        loc_dic[c[0]]=(c[1],c[2])
+        data.append({"name":c[0],"value":c[3]})
+    return {"location":loc_dic,"data":data}
+
+def get_CATE_UK(limit=15):
+    cu.execute("select * from CATE_UK limit "+str(limit))
+    loc_dic={}
+    data=[]
+    for c in cu.fetchall():
+        loc_dic[c[0]]=(c[1],c[2])
+        data.append({"name":c[0],"value":c[3]})
+    return {"location":loc_dic,"data":data}
+
+def get_WORK_HOURS():
+    cu.execute("select * from WORK_HOURS ")
     loc_dic={}
     data=[]
     for c in cu.fetchall():
