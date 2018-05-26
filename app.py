adb53ce5fc7d33a12e3ccc64deec565fa64efdc4
YuanGao0618 <gaoyuan0618syd@gmail.com>
2018-05-26 12:47:59 +1000

    table api change

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, jsonify
+from flask_restful import Resource, Api, reqparse, abort
+from flask_cors import CORS
+import connectdata
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/city_job_au", methods=['GET'])
+def get_city_au():
+    return jsonify(connectdata.get_CITY_AU()),200
+
+
+@app.route("/city_job_uk", methods=['GET'])
+def get_city_uk():
+    return jsonify(connectdata.get_CITY_UK()),200
+
+
+@app.route("/city_job_au/<name>", methods=['GET'])
+def retrieve_cityau(name):
+    return jsonify(connectdata.retrive_CITY_AU(name)),200
+    
+@app.route("/city_job_uk/<name>", methods=['GET'])
+def retrieve_cityuk(name):
+    return jsonify(connectdata.retrive_CITY_UK(name)),200
+
+
+@app.route("/city_au/<name>", methods=['GET'])
+def get_au(name):
+    return jsonify(connectdata.get_CITY_AU(name)),200
+
+@app.route("/city_uk/<name>", methods=['GET'])
+def get_uk(name):
+    return jsonify(connectdata.get_CITY_UK(name)),200
+
+@app.route("/work_hours", methods=['GET'])
+def get_work_hours():
+    return jsonify(connectdata.get_WORK_HOURS()),200
+
+@app.route("/industryAu", methods=['GET'])
+def get_industry_au():
+    return jsonify(connectdata.get_CATE_AU()),200
+
+@app.route("/industryUk", methods=['GET'])
+def get_industry_uk():
+    return jsonify(connectdata.get_CATE_UK()),200
+
+@app.route("/informationau/<name>", methods=['GET'])
+def get_au_information(name):
+    return jsonify(connectdata.information_au(name)),200
+
+@app.route("/informationuk/<name>", methods=['GET'])
+def get_uk_information(name):
+    return jsonify(connectdata.information_uk(name)),200
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Accept,Content-Type,AUTH_TOKEN')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+if __name__ == "__main__":
+    app.run()
---
+++ connectdata.py
@@ -0,0 +1,182 @@
+import sqlite3
+import os
+import operator
+
+
+dbpath = os.getcwd()+os.path.sep+"database"+os.path.sep+"COMP9313_Ass3.db"
+conn = sqlite3.connect(dbpath,check_same_thread=False)
+
+cu=conn.cursor()
+
+#{location: {'city1':[x,y], 'city2':[x,y],...}, data:[{name: 'city1', value:233}, {name: 'city2', value:500}]}
+def get_CITY_AU(city_name="all",limit=15):
+    if city_name=="all":
+        cu.execute("select * from CITY_AU ORDER BY job_amount DESC limit "+str(limit))
+    else:
+        cu.execute(f'select * from CITY_AU where LOWER(city) = \'{city_name.lower()}\'')
+    loc_dic={}
+    data=[]
+    for c in cu.fetchall():
+        loc_dic[c[0]]=(c[2],c[1])
+        data.append({"name":c[0],"value":c[3]})
+    return {"location":loc_dic,"data":data}
+
+
+def get_CITY_UK(city_name="all",limit=15):
+    if city_name=="all":
+        cu.execute("select * from CITY_UK ORDER BY job_amount DESC limit "+str(limit))
+    else:
+        cu.execute(f'select * from CITY_UK where LOWER(city) = \'{city_name.lower()}\'')
+    loc_dic={}
+    data=[]
+    for c in cu.fetchall():
+        loc_dic[c[0]]=(c[2],c[1])
+        data.append({"name":c[0],"value":c[3]})
+    return {"location":loc_dic,"data":data}
+
+def get_CATE_AU(limit=15):
+    '''
+    cu.execute("select * from CATE_AU limit "+str(limit))
+    loc_dic={}
+    data=[]
+    for c in cu.fetchall():
+        loc_dic[c[0]] = c[1]
+    industry = sorted(loc_dic.items(), key=operator.itemgetter(1))
+    return {"Industry":loc_dic}
+    '''
+    cu.execute("select * from CATE_AU")
+    loc_dic={}
+    data=[]
+    category = []
+    for c in cu.fetchall():
+        category.append(c[0])
+        data.append({"value":c[1], "name" : c[0]})
+    # industry = sorted(loc_dic.items(), key=operator.itemgetter(1))
+    return {"data":category, "value":data}
+
+
+
+def get_CATE_UK(limit=15):
+    '''
+    cu.execute("select * from CATE_UK limit "+str(limit))
+    loc_dic={}
+    data=[]
+    for c in cu.fetchall():
+        loc_dic[c[0]]=(c[1],c[2])
+        data.append({"name":c[0],"value":c[3]})
+    return {"location":loc_dic,"data":data}
+    '''
+    cu.execute("select * from CATE_UK")
+    loc_dic={}
+    data=[]
+    category = []
+    for c in cu.fetchall():
+        category.append(c[0])
+        data.append({"value":c[1], "name" : c[0]})
+    # industry = sorted(loc_dic.items(), key=operator.itemgetter(1))
+    return {"data":category, "value":data}
+
+#{"category":[],"male_au":[],"female_au":[],"male_uk":[],"female_uk":[]}
+
+def get_WORK_HOURS():
+    cu.execute("select * from WORK_HOURS where sex != 'Total'")
+    data=[]
+    for c in cu.fetchall():
+        data.append(c)
+    data = sorted(data, key = lambda item:item[2])
+    category=[]
+    male_au=[]
+    female_au=[]
+    male_uk=[]
+    female_uk=[]
+    for t in data:
+        if t[2] not in category:
+            category.append(t[2])
+        if t[0] == "AU":
+            if t[1] == "Female":
+                female_au.append(-t[3])
+            else:
+                male_au.append(-t[3])
+        else:
+            if t[1] == "Female":
+                female_uk.append(t[3])
+            else:
+                male_uk.append(t[3])
+    return {"category":category,"male_au":male_au,"female_au":female_au,"male_uk":male_uk,"female_uk":female_uk}
+
+def retrive_CITY_AU(name):
+    select_query = "SELECT city FROM CITY_AU"
+    city_au = []
+    choosen_city = []
+    for row in cu.execute(select_query).fetchall():
+        city_au.append(row[0])
+    x = len(name)
+    name = name[0].upper() + name[1:].lower()
+    for i in city_au:
+        if i[0:x] == name:
+            choosen_city.append(i)
+        if len(choosen_city)>5:
+            break
+    return {"City":choosen_city}
+
+def retrive_CITY_UK(name):
+    select_query = "SELECT city FROM CITY_UK"
+    city_uk = []
+    choosen_city = []
+    for row in cu.execute(select_query).fetchall():
+        city_uk.append(row[0])
+    x = len(name)
+    name = name[0].upper() + name[1:].lower()
+    for i in city_uk:
+        if i[0:x] == name:
+            choosen_city.append(i)
+        if len(choosen_city)>5:
+            break
+    return {"City":choosen_city}
+
+def takeSecond(elem):
+    return elem[1]
+
+def information_au(name):
+
+    select_query = "SELECT city,category FROM job_au"
+    city_au = []
+
+    for row in cu.execute(select_query).fetchall():
+        if row[0].lower() == name.lower():
+            city_au.append(row[1])
+    myset = set(city_au)
+    au_city = []
+    for item in myset:
+        au_city.append([item,city_au.count(item)])
+    au_city.sort(key=takeSecond)
+    au_city.reverse()
+    return {"Data":au_city}
+
+
+
+
+def information_uk(name):
+    select_query = "SELECT city,category FROM job_uk"
+    city_uk = []
+    for row in cu.execute(select_query).fetchall():
+        if row[0].lower() == name.lower:
+            city_uk.append(row[1])
+    myset = set(city_uk)
+    uk_city = []
+    for item in myset:
+        uk_city.append([item,city_uk.count(item)])
+    uk_city.sort(key=takeSecond)
+    uk_city.reverse()
+    return {"Data":uk_city}
+
+
+
+
+
+
+
+
+
+
+
